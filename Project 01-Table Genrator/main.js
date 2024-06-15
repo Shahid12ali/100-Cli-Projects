d3d80de3a6dfa257fb75dd02c86fc91830c176b0
Shahid Ali <153486921+Shahid12ali@users.noreply.github.com>
2024-06-15 12:57:10 +0500

    Table_Genrator

---
+++ Project 01-Table Genrator/main.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let table = true;
+while (table) {
+    let userInput = await inquirer.prompt([
+        {
+            name: "num",
+            type: "number",
+            message: "Enter Your Number To Genrate Table",
+        }
+    ]);
+    let myNumber = userInput.num;
+    if (myNumber) {
+        console.log(`Here Is The Table Of ${myNumber}\n`);
+        for (let i = 1; i <= 10; i++) {
+            console.log(`${myNumber} x  ${i} = ${myNumber * i}`);
+        }
+    }
+    else {
+        console.log(`Enter A Numerical Form`);
+    }
+    let againPrint = await inquirer.prompt([
+        {
+            name: "askUser",
+            type: "confirm",
+            message: "Do You Want To Print Another Table",
+            default: false,
+        }
+    ]);
+    if (againPrint.askUser === false) {
+        table = false;
+        console.log(`\nThankyou `);
+    }
+}
